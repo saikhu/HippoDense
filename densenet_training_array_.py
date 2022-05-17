99165c66fd20e49b653b88395af64678ea078db6
Usman Asim <asimsaikhu@gmail.com>
2022-05-17 08:16:21 +0000

    update

--- densenet_training_array_.py
+++ densenet_training_array_.py
@@ -36,29 +36,47 @@ from monai.transforms import (
 logging.basicConfig(stream=sys.stdout, level=logging.INFO)
 # print_config()
 
+
+
 # set this in your environment or previous cell to wherever IXI is downloaded and extracted
-root_dir = "/workspace/monai/run_3d_classification"
-dataset_dir = "/raid/01_P_Classification_all/"
-
-images = []
-for root, dirs, files in os.walk(os.path.abspath(dataset_dir)):
+RunFolder = "/HippoDense/runFolder"
+DatasetDir = "/Datasets/01_P_Classification_all_hippo"
+Labels = []
+Images = []
+NotFitImages = []
+for root, dirs, files in os.walk(os.path.abspath(DatasetDir)):
     for file in files:
-        images.append(os.path.join(root, file))
-
-
+        if file.split("_")[-3] == "L":
+            Labels.append(2)
+            Images.append(os.path.join(root, file))
+        elif file.split("_")[-3] == "E":
+            Labels.append(1)
+            Images.append(os.path.join(root, file))
+        elif file.split("_")[-2] == "Normal":
+            Labels.append(0)
+            Images.append(os.path.join(root, file))
+        elif file.split("_")[-2] == "Dementia":
+            Labels.append(3)
+            Images.append(os.path.join(root, file))
+        else:
+            NotFitImages.append(file)
+            # print(file)
+print("{}: images cannot be used.".format(len(NotFitImages)))
+# # labels = np.load(
+#     '/workspace/monai/MONAI-tutorials/3d_classification/01_P_Classification_all_labels.npy')
+NumClasses = len(np.unique(Labels))
+# ClassLabels = np.array(Labels)
+if len(Images)==len(Labels):
+    print("{}: total images prepared with {} different classes".format(len(Images), NumClasses))
 # images
 
 
-labels = np.load(
-    '/workspace/monai/MONAI-tutorials/3d_classification/01_P_Classification_all_labels.npy')
-numClasses = np.unique(labels)
-print(len(images)==len(labels))
 # Represent labels in one-hot format for binary classifier training,
 # BCEWithLogitsLoss requires target to have same shape as input
 pin_memory = torch.cuda.is_available()
 
-labels = torch.nn.functional.one_hot(torch.as_tensor(labels)).float()
-
+labels = torch.nn.functional.one_hot(torch.as_tensor(Labels)).float()
+print(labels.shape)
 # Define transforms
 train_transforms = Compose([ScaleIntensity(), AddChannel(), Resize((224, 256, 208)), RandRotate90(), EnsureType()])
 # train_transforms = Compose([ScaleIntensity(), AddChannel(), EnsureType()])
@@ -69,21 +87,20 @@ val_transforms = Compose(
     # [ScaleIntensity(), AddChannel(), EnsureType()])
 
 # Define nifti dataset, data loader
-check_ds = ImageDataset(image_files=images, labels=labels, transform=train_transforms)
-check_loader = DataLoader(check_ds, batch_size=1, num_workers=16, pin_memory=pin_memory)
+check_ds = ImageDataset(image_files=Images, labels=labels, transform=train_transforms)
+check_loader = DataLoader(check_ds, batch_size=1, num_workers=64, pin_memory=pin_memory)
 
 im, label = monai.utils.misc.first(check_loader)
 print(type(im), im.shape, label, label.shape)
 
+
 # create a training data loader
-train_ds = ImageDataset(image_files=images[:-66], labels=labels[:-66], transform=train_transforms)
-train_loader = DataLoader(train_ds, batch_size=1, shuffle=True, num_workers=16, pin_memory=pin_memory)
+train_ds = ImageDataset(image_files=Images[:-66], labels=labels[:-66], transform=train_transforms)
+train_loader = DataLoader(train_ds, batch_size=1, shuffle=True, num_workers=64, pin_memory=pin_memory)
 
 # create a validation data loader
-val_ds = ImageDataset(image_files=images[-66:], labels=labels[-66:], transform=val_transforms)
-val_loader = DataLoader(val_ds, batch_size=1, num_workers=16, pin_memory=pin_memory)
-
-
+val_ds = ImageDataset(image_files=Images[-66:], labels=labels[-66:], transform=val_transforms)
+val_loader = DataLoader(val_ds, batch_size=1, num_workers=32, pin_memory=pin_memory)
 
 # Create DenseNet121, CrossEntropyLoss and Adam optimizer
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
@@ -103,7 +120,7 @@ Args:
 """
 
 model = monai.networks.nets.DenseNet121(
-    spatial_dims=3, in_channels=1, out_channels=len(numClasses)).to(device)
+    spatial_dims=3, in_channels=1, out_channels=NumClasses).to(device)
 
 # loss_function = torch.nn.CrossEntropyLoss()
 loss_function = torch.nn.BCEWithLogitsLoss()  # also works with this data
@@ -117,7 +134,7 @@ best_metric_epoch = -1
 epoch_loss_values = []
 metric_values = []
 writer = SummaryWriter()
-max_epochs = 70
+max_epochs = 300
 
 for epoch in range(max_epochs):
     print("-" * 10)
@@ -163,11 +180,8 @@ for epoch in range(max_epochs):
             best_metric = metric
             best_metric_epoch = epoch + 1
             timeNow = time.strftime("%Y%m%d_%H%M%S")
-            bestModelstatePath = os.path.join(root_dir, "best_metric_model_classification3d_array" + timeNow + ".pth")
-            torch.save(model.state_dict(), bestModelstatePath)
-            bestmodelPath = os.path.join(
-                root_dir, "best_metric_model_classification3d_array" + timeNow + ".pt")
-            torch.save(model, bestmodelPath)
+            bestModelPath = os.path.join(RunFolder, "best_metric_model_classification3d_array_" + timeNow + ".pth")
+            torch.save(model.state_dict(), bestModelPath)
             print("saved new best metric model")
 
         print(f"Current epoch: {epoch+1} current accuracy: {metric:.4f} ")
@@ -177,60 +191,8 @@ for epoch in range(max_epochs):
 print(f"Training completed, best_metric: {best_metric:.4f} at epoch: {best_metric_epoch}")
 writer.close()
 
-
-
-# create a validation data loader
-test_ds = ImageDataset(image_files=images[-10:], labels=labels[-10:], transform=val_transforms)
-test_loader = DataLoader(val_ds, batch_size=1, num_workers=2, pin_memory=torch.cuda.is_available())
-itera = iter(test_loader)
-
-
-def get_next_im():
-    test_data = next(itera)
-    return test_data[0].to(device), test_data[1].unsqueeze(0).to(device)
-
-
-def plot_occlusion_heatmap(im, heatmap):
-    plt.subplots(1, 2)
-    plt.subplot(1, 2, 1)
-    plt.imshow(np.squeeze(im.cpu()))
-    plt.colorbar()
-    plt.subplot(1, 2, 2)
-    plt.imshow(heatmap)
-    plt.colorbar()
-    plt.show()
-    imageSavePath = os.path.join(root_dir, "best_metric_model_classification3d_array" + timeNow + ".png")
-    plt.savefig("")
-
-
-
-# Get a random image and its corresponding label
-img, label = get_next_im()
-
-
-# Get the occlusion sensitivity map
-occ_sens = monai.visualize.OcclusionSensitivity(nn_module=model,  n_batch=10, stride=12)
-# Only get a single slice to save time.
-# For the other dimensions (channel, width, height), use
-# -1 to use 0 and img.shape[x]-1 for min and max, respectively
-depth_slice = img.shape[2] // 2
-occ_sens_b_box = [-1, -1, depth_slice, depth_slice, -1, -1, -1, -1]
-
-occ_result, _ = occ_sens(x=img, b_box=occ_sens_b_box)
-occ_result = occ_result[..., label.argmax().item()]
-
-fig, axes = plt.subplots(1, 2, figsize=(25, 15), facecolor="white")
-
-for i, im in enumerate([img[:, :, depth_slice, ...], occ_result]):
-    cmap = "gray" if i == 0 else "jet"
-    ax = axes[i]
-    im_show = ax.imshow(np.squeeze(im[0][0].detach().cpu()), cmap=cmap)
-    ax.axis("off")
-    fig.colorbar(im_show, ax=ax)
-fig.savefig('image.svg')
-
-
-
-
-
-
+import pickle
+with open('clf.pickle', 'wb') as f:
+    pickle.dump(model, f)
+# modelPath = "/workspace/monai/run_3d_classification/best_metric_model_classification3d_array20220415_063943.pt"
+# model = torch.load(modelPath)
